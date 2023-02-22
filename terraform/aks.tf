17a7aa5ba91573d4cd2fb708f5e774d6aa67e41e
msftph <65667413+msftph@users.noreply.github.com>
2023-02-22 16:44:32 -0500

    adds alert

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -17,15 +17,17 @@ resource "azurerm_kubernetes_cluster" "default" {
     type = "SystemAssigned"
   }
 
+  # these lines turns on container insights
   oms_agent {
     log_analytics_workspace_id = azurerm_log_analytics_workspace.default.id
   }
 }
 
 # see https://learn.microsoft.com/en-us/azure/azure-monitor/containers/container-insights-custom-metrics?tabs=cli#enable-custom-metrics
-# the preview feature for role assignment this will be added automatically
-# for now we need to ad it manually
-# the id is hard to find, it is part of the "oms_agent" output attribute and was moved from addon_profiles in the later version of the providers
+# The preview feature for role assignment this will be added automatically.
+# For now we need to ad it manually.
+# The id is hard to find, it is part of the "oms_agent" output attribute and was moved from addon_profiles in the later version of the providers.
+# Use object_id and specify check AAD otherwise it will go into an infinite loop.
 # https://github.com/hashicorp/terraform-provider-azurerm/pull/7056
 resource "azurerm_role_assignment" "omsagent-aks" {
   scope                            = azurerm_kubernetes_cluster.default.id
--- terraform/alerts.tf
+++ terraform/alerts.tf
@@ -1,25 +1,39 @@
-
-/* resource "azurerm_monitor_metric_alert" "example" {
-  name                = "KubePodCrashLooping"
+// turn on monitor alerts perview in the portal and run
+//
+// ```bash
+// az monitor metrics alert list -g aks
+// ```
+// 
+// take the result json and map it to terraform
+// perhaps write a terraform for_each loop over json
+resource "azurerm_monitor_metric_alert" "example" {
+  name                = "Completed job count"
   resource_group_name = azurerm_resource_group.default.name
   scopes              = [azurerm_kubernetes_cluster.default.id]
-  description         = "Detect when a pod is crash looping."
+  description         = "This alert monitors completed jobs (more than 6 hours ago)."
 
   criteria {
-    metric_namespace = "Microsoft.Storage/storageAccounts"
-    metric_name      = "Transactions"
-    aggregation      = "Total"
+    metric_namespace = "Insights.Container/pods"
+    metric_name      = "completedJobsCount"
+    aggregation      = "Average"
     operator         = "GreaterThan"
-    threshold        = 50
+    threshold        = 0.0
+
 
     dimension {
-      name     = "ApiName"
+      name     = "controllerName"
+      operator = "Include"
+      values   = ["*"]
+    }
+
+    dimension {
+      name     = "kubernetes namespace"
       operator = "Include"
       values   = ["*"]
     }
   }
 
-  action {
-    action_group_id = azurerm_monitor_action_group.main.id
-  }
-} */
+  #   action {
+  #     action_group_id = azurerm_monitor_action_group.main.id
+  #   }
+} 
