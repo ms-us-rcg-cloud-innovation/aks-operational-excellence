078795cda237bbcb27c72d55bb6a072ff4f9af90
Samir Banjanovic <2653176+samirbanjanovic@users.noreply.github.com>
2023-02-28 10:18:01 -0500

    Aks env deploy (#4)
    * Added environmental var files for sandbox and development
    
    * Finishing deployment workflow that reuses workflows
    
    * Finished aks deployment pipeline running on environment configs and vars file

---
+++ terraform/aks/environment-vars/development.tfvars
@@ -0,0 +1,4 @@
+name               = "k8s_test_dev"
+location           = "eastus"
+kubernetes_version = "1.24.6"
+default_pool_size  = 1
--- terraform/aks/variables.tf
+++ terraform/aks/variables.tf
@@ -1,17 +1,19 @@
 variable "name" {
-  type    = string
+  type        = string
   description = "Name of the AKS cluster"
-  default = "aks"
 }
 
 variable "location" {
-  type    = string
+  type        = string
   description = "Location of AKS cluster"
-  default = "East US 2"
 }
 
 variable "kubernetes_version" {
-  type = string
+  type        = string
   description = "Version of the kubernetes cluster to deploy"
 }
 
+variable "default_pool_size" {
+  type        = number
+  description = "Number of cluster nodes in default pool"
+}
