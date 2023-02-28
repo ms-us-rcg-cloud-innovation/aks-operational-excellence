71741bdd7b83d6a2f2fb00b196e1881b5aaea2e2
Samir Banjanovic <sbanj17@gmail.com>
2023-02-27 20:38:14 -0500

    Working on clsuter upgrade through TF

--- terraform/aks/variables.tf
+++ terraform/aks/variables.tf
@@ -1,9 +1,15 @@
 variable "name" {
   type    = string
+  description = "Name of the AKS cluster"
   default = "aks"
 }
 
 variable "location" {
   type    = string
+  description = "Location of AKS cluster"
   default = "East US 2"
+}
+
+variable "kubernetes_version" {
+  type = string
 }
