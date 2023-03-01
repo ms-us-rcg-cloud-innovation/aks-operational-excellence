12aad259db601eb56cad94212efc2328a91e33c6
Samir Banjanovic <sbanj17@gmail.com>
2023-03-01 14:37:55 -0500

    Proper terraform formatting

--- terraform/aks/variables.tf
+++ terraform/aks/variables.tf
@@ -24,11 +24,11 @@ variable "default_pool_size" {
 }
 
 variable "node_surge" {
-  type = string
+  type        = string
   description = "The maximum number or percentage of nodes which will be added to the default Node Pool size during an upgrade"
-  default = "33%"
+  default     = "33%"
   validation {
-    condition = can(regex("^[0-9]+[%]?$", var.node_surge))
+    condition     = can(regex("^[0-9]+[%]?$", var.node_surge))
     error_message = "Value can be a positive integer or percentage"
   }
 }
